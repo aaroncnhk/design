0dae5e3d876317d73923863389de61be7406d39f
aaroncnhk <841089370@qq.com>
2016-02-02 15:55:56 +0800

    controller update
    controller update

--- backend/controllers/ArticleController.php
+++ backend/controllers/ArticleController.php
@@ -2,13 +2,14 @@
 namespace backend\controllers;
 
 use Yii;
-use yii\filters\AccessControl;
+// use yii\filters\AccessControl;
 use yii\web\Controller;
-use common\models\LoginForm;
-use yii\filters\VerbFilter;
+// use common\models\LoginForm;
+// use yii\filters\VerbFilter;
 
 /**
- * Site controller
+ * article controller
+ * @author sun <7795346@qq.com>
  */
 class ArticleController extends Controller
 {
